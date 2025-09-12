6bccceba2fc800b82468fd42fbfc9be01a389a3e
Aaron Johnson <aaronis@fastmail.com>
2025-09-12 14:03:16 -0500

    splash page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,3 +14,9 @@
 <Header />
 {@render children?.()}
 <Footer />
+
+<div class="fixed top-0 left-0 z-[999999999999999999999999999] h-full w-full bg-darkBackground">
+	<div class="container_xl relative flex h-full items-center justify-center px-5">
+		<p class="relative text-center font-rubik text-6xl font-bold text-white">Coming Soon!</p>
+	</div>
+</div>
