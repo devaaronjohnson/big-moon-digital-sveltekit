f1ad8d1fd5d589a0f8533ae80963921f3919e5ee
Aaron Johnson <aaronis@fastmail.com>
2025-09-12 15:02:42 -0500

    thank you page

---
+++ src/routes/thank-you/+page.svelte
@@ -0,0 +1,16 @@
+<main class="bg-darkBackground">
+	<section class="pt-40 pb-40 lg:pt-80 lg:pb-56">
+		<div class="container_md flex-col items-center justify-center px-5">
+			<p class="text-center font-rubik text-4xl leading-tight font-medium text-white lg:text-5xl">
+				Thanks for contacting us. I will be in touch within 24 hours.
+			</p>
+			<div class="mt-8 flex items-center justify-center">
+				<a
+					href="/"
+					class="rounded-full border border-darkestBlue bg-darkestBlue px-8 py-3 font-karla text-base font-normal text-white uppercase transition-all duration-200 hover:border-darkBlue hover:bg-darkBlue"
+					>Back Home</a
+				>
+			</div>
+		</div>
+	</section>
+</main>
